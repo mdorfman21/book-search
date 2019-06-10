165a91e9a9c97cfde3fe7a51580dae70aead9f32
Morgan Dorfman <mdorfman21@gmail.com>
2019-06-09 22:49:42 -0400

    updated mongod uri

---
+++ client/build/precache-manifest.c27776ef3dbb2f487e0d33053bf4d766.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79300bf3f0007cce55f3",
+    "url": "/static/js/main.ebf83444.chunk.js"
+  },
+  {
+    "revision": "dde67915622850c0e668",
+    "url": "/static/js/2.957ad680.chunk.js"
+  },
+  {
+    "revision": "79300bf3f0007cce55f3",
+    "url": "/static/css/main.da518927.chunk.css"
+  },
+  {
+    "revision": "529669145246a4849e6e9e1f1e665527",
+    "url": "/index.html"
+  }
+];
